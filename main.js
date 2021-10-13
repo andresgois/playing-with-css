9d148d554cdff230abc2963d085a1fd2367dc9a8
Andre gois <andre.s.gois3@gmail.com>
2021-10-13 19:53:45 -0300

    controlling states

--- main.js
+++ main.js
@@ -2,13 +2,26 @@
   window.addEventListener("load", function(event) {
     
     let flex = document.querySelector("#flex");
+    let alignFlex = document.querySelector("#align-flex");
+    let flexDirection = document.querySelector("#flex-direction");
+    let flexWrap = document.querySelector("#flex-wrap");
+    let flexFlow = document.querySelector("#flex-flow");
+    let justifyContent = document.querySelector("#justify-content");
+    let alignItems = document.querySelector("#align-items");
+
     let block = document.querySelector(".block");
     let box = document.querySelector(".box");
     
     flex.addEventListener("change", () => {
-      console.log(flex.value)
       block.style.display = flex.value
     });
+    
+    justifyContent.addEventListener("change", () => {
+      block.style.justifyContent = justifyContent.value
+    });
 
+    flexDirection.addEventListener("change", () => {
+      block.style.flexDirection = flexDirection.value
+    });
 
   });
