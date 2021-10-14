539fc6a540e445bb9ebfe80e45722a3ff63ac904
Andre gois <andre.s.gois3@gmail.com>
2021-10-14 19:33:59 -0300

    aligning the content

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
 
   window.addEventListener("load", function(event) {
     
-    let flex = document.querySelector("#flex");
+    let display = document.querySelector("#flex");
     let alignFlex = document.querySelector("#align-flex");
     let flexDirection = document.querySelector("#flex-direction");
     let flexWrap = document.querySelector("#flex-wrap");
     let flexFlow = document.querySelector("#flex-flow");
     let justifyContent = document.querySelector("#justify-content");
     let alignItems = document.querySelector("#align-items");
+    let alignContent = document.querySelector("#align-content");
 
     let block = document.querySelector(".block");
     let box = document.querySelector(".box");
     
-    flex.addEventListener("change", () => {
-      block.style.display = flex.value
+    display.addEventListener("change", () => {
+      block.style.display = display.value
+    });
+    
+    flexDirection.addEventListener("change", () => {
+      block.style.flexDirection = flexDirection.value
+    });
+
+    flexWrap.addEventListener("change", () => {
+      block.style.flexWrap = flexWrap.value
     });
     
     justifyContent.addEventListener("change", () => {
       block.style.justifyContent = justifyContent.value
     });
 
-    flexDirection.addEventListener("change", () => {
-      block.style.flexDirection = flexDirection.value
+    flexFlow.addEventListener("change", () => {
+      block.style.flexFlow = flexFlow.value
     });
+
+    alignItems.addEventListener("change", () => {
+      block.style.alignItems = alignItems.value
+    });
+    
+    alignContent.addEventListener("change", () => {
+      block.style.alignContent = alignContent.value
+    });
+
 
   });
