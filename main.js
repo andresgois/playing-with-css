2f901fc81e77e1d16107053833d91bda718822e8
Andre gois <andre.s.gois3@gmail.com>
2021-11-07 18:35:14 -0300

    adding grid

--- main.js
+++ main.js
@@ -2,20 +2,11 @@
   window.addEventListener("load", function(event) {
     
     let display = document.querySelector("#flex");
-    let alignFlex = document.querySelector("#align-flex");
     let flexDirection = document.querySelector("#flex-direction");
     let flexWrap = document.querySelector("#flex-wrap");
     let justifyContent = document.querySelector("#justify-content");
     let alignItems = document.querySelector("#align-items");
     let alignContent = document.querySelector("#align-content");
-    let flexFlow = document.querySelector("#flex-flow");
-
-    let flexGrow = document.querySelector("#flex-grow");
-    let flexBasis = document.querySelector("#flex-basis");
-    let flexShrink = document.querySelector("#flex-shrink");
-    let flex = document.querySelector("#flex");
-    let order = document.querySelector("#order");
-    let alignSelf = document.querySelector("#align-self");
 
     let block = document.querySelector(".block");
     let box1 = document.querySelector(".box-1");
@@ -23,6 +14,7 @@
     let box3 = document.querySelector(".box-3");
     let box4 = document.querySelector(".box-4");
     let p_flex = this.document.querySelector('.p-flex');
+    let p_grid = this.document.querySelector('.p-grid');
     
 
     display.addEventListener("change", () => {
@@ -30,9 +22,11 @@
       if(display.value === 'flex'){        
         //p_flex.classList.contains("hidden");
         p_flex.classList.remove('hidden')
-      }else{
-        p_flex.classList.add('hidden')
+      }else if(display.value === 'grid'){
+          p_flex.classList.add('hidden')
+          p_grid.classList.remove('hidden2')          
       }
+      
     });
     
     flexDirection.addEventListener("change", () => {
@@ -54,58 +48,21 @@
     alignContent.addEventListener("change", () => {
       block.style.alignContent = alignContent.value
     });
-    /*
 
-    flexFlow.addEventListener("change", () => {
-      block.style.flexFlow = flexFlow.value
-    });
+    box1.addEventListener("keyup", () => {
+      document.querySelector(".box1").style.order = box1.value
+    })
+    box2.addEventListener("keyup", () => {
+      document.querySelector(".box2").style.order = box2.value
+    })
+    box3.addEventListener("keyup", () => {
+      document.querySelector(".box3").style.order = box3.value
+    })
+    box4.addEventListener("keyup", () => {
+      document.querySelector(".box4").style.order = box4.value
+    })
 
-    
 
-    flexGrow.addEventListener("change", () => {
-      block.style.flexGrow = flexGrow.value
-    });
-    flexBasis.addEventListener("change", () => {
-      block.style.flexBasis = flexBasis.value
-    });
-
-    flexShrink.addEventListener("change", () => {
-      block.style.flexShrink = flexShrink.value
-    });
-
-    flex.addEventListener("change", () => {
-      block.style.flex = flex.value
-    });
-    // alignContent.addEventListener("change", () => {
-    //   block.style.alignContent = alignContent.value
-    // });
-    alignSelf.addEventListener("change", () => {
-      block.style.alignSelf = alignSelf.value
-    });
-
-    if(box1){
-      box1.addEventListener("keyup", () => {
-        document.querySelector(".box1").style.order = box1.value
-      })
-    }
-
-    if(box2){
-      box2.addEventListener("keyup", () => {
-        document.querySelector(".box2").style.order = box2.value
-      })
-    }
-
-    if(box3){
-      box3.addEventListener("keyup", () => {
-        document.querySelector(".box3").style.order = box3.value
-      })
-    }
-
-    if(box4){
-      box4.addEventListener("keyup", () => {
-        document.querySelector(".box4").style.order = box4.value
-      })
-    }*/
 
 
   });
